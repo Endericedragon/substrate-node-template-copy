5968050d6e9421328279774fd7a4d29f2ab9a9b3
Endericedragon Fu <fuze1007@163.com>
2023-12-16 21:30:24 +0800

    开始研究依赖关系

--- node/build.rs
+++ node/build.rs
@@ -1,7 +1,8 @@
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
+	// 获取并打印一些版本信息
 	generate_cargo_keys();
-
+	// 从$CARGO_MANIFEST_DIR开始，逐层向上检查git仓库目录
 	rerun_if_git_head_changed();
 }
